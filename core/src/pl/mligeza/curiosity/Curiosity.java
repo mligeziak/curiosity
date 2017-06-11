4b000a6f65966032c9810ce9ca92da80ab253a2a
Hubert Kurek <hkurkov@gmail.com>
2017-06-11 12:54:33 +0200

    Updated gradle to java 1.8, using inputmanager for mouse input instead of default libgdx input

--- core/src/pl/mligeza/curiosity/Curiosity.java
+++ core/src/pl/mligeza/curiosity/Curiosity.java
@@ -2,50 +2,53 @@ package pl.mligeza.curiosity;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector3;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Curiosity extends ApplicationAdapter {
     public static final String TAG = "[Curiosity]";
 
     private OrthographicCamera camera = new OrthographicCamera();
     private SpriteBatch spriteBatch;
+
+    public static Sprite defaultImg;
     private Texture img;
+
+    private List<Sprite> sprites = new ArrayList<>();
 
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
+
+        defaultImg = new Sprite(img);
+
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//        Gdx.input.setInputProcessor(new InputManager());
+        Gdx.input.setInputProcessor(new InputManager(camera, sprites));
+    }
+
+    public void update(float dt) {
     }
 
     @Override
     public void render() {
+        update(Gdx.graphics.getDeltaTime());
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            Gdx.app.exit();
-        }
-
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            Gdx.app.log(TAG, "Mouse left clicked");
-        }
 
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
         {
-            Vector3 mousePos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(mousePos);
-            int w = img.getWidth();
-            int h = img.getHeight();
-
-            spriteBatch.draw(img, mousePos.x - (w / 2), mousePos.y - (h / 2));
+            for (Sprite sprite : sprites) {
+                sprite.draw(spriteBatch);
+            }
         }
         spriteBatch.end();
     }
--- core/src/pl/mligeza/curiosity/InputManager.java
+++ core/src/pl/mligeza/curiosity/InputManager.java
@@ -1,35 +1,66 @@
 package pl.mligeza.curiosity;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.List;
 
 public class InputManager extends InputAdapter {
+    private OrthographicCamera camera;
+    private List<Sprite> sprites;
+
+    public InputManager(OrthographicCamera camera, List<Sprite> sprites) {
+        this.camera = camera;
+        this.sprites = sprites;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (Input.Keys.ESCAPE == keycode) {
+            Gdx.app.exit();
+        }
+
+        return true;
+    }
+
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("mouse down");
+        Sprite newSprite = new Sprite(Curiosity.defaultImg);
+
+        Vector3 newPosition = new Vector3(screenX, screenY, 0);
+        camera.unproject(newPosition);
+
+        float wHalf = newSprite.getWidth() / 2;
+        float hHalf = newSprite.getHeight() / 2;
+        newSprite.setPosition(newPosition.x - wHalf, newPosition.y - hHalf);
+
+        sprites.add(newSprite);
+
         return true;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        System.out.println("mouse up");
         return true;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        System.out.println("mouse dragged");
         return true;
     }
 
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
-        System.out.println("mouse moved");
+
         return true;
     }
 
     @Override
     public boolean scrolled(int amount) {
-        System.out.println("scroll moved: " + amount);
         return true;
     }
 }
